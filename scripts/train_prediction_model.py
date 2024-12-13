c5e133bd3c8c5013ccde710b6593248439349de9
inu-wanwan <rixy.takagi@gmail.com>
2024-12-13 20:29:27 +0900

    Pending construction of training scripts as we realized that we need to split the dataset for testing and training.

--- scripts/train_prediction_model.py
+++ scripts/train_prediction_model.py
@@ -1,14 +1,23 @@
 import sys
 import os
-from utils import load_config
+import wandb
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+from utils import load_config
 from src.data.dataloader import get_dataloader
 from src.score_prediction_models.docking_score_predictor import DockingScorePredictor
 
 # load the configuration
 file_config = load_config('filepath.yml')
 data_config = load_config('data.yml')
-model_config = load_config('model.yml')
+model_config = load_config('model.yml')['sample_model']
+train_config = load_config('train.yml')['sample_model']
+
+print(f"model_config: {model_config}")
+print(f"train_config: {train_config}")
 
 
 alphafold_dir = file_config['data']['alphafold']
@@ -17,7 +26,7 @@ data_dir = file_config['data']['samples']
 smiles_max_len = data_config['dataset']['smiles_max_len']
 protein_max_len = data_config['dataset']['protein_max_len']
 
-batch_size = model_config['sample_model']['batch_size']
+batch_size = train_config['batch_size']
 
 dataloader = get_dataloader(
     csv_file=os.path.join(data_dir, 'sample_dataset.csv'),
@@ -37,3 +46,10 @@ for batch_idx, batch in enumerate(dataloader):
     print(f"    Protein mask; {batch['protein_mask'][0]}")
     print(f"    Docking score: {batch['docking_score']}")
     break
+
+def train():
+    # initialize WandB
+    wandb.init(project='sample_model', config=train_config)
+
+    # load dataset
+    
